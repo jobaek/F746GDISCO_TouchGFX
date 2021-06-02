0d41f7280b52012ff1143211eb8f0dda6bfc2430
D:\Work\GitHub <jobaek78@gmail.com>
2021-06-02 22:35:03 +0900

    STM32CubeIDE v1.6.1 STM32CubeMX : v6.2.1 HAL : STM32Cube_F7 v1.16.1 TouchGFX : v4.16.1

---
+++ TouchGFX/generated/fonts/src/Kerning_verdana_40_4bpp.cpp
@@ -0,0 +1,20 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_verdana_40_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE =
+{
+    { 0x0043, -1 },  // (First char = [0x0043, C], Second char = [0x002D, -], Kerning dist = -1)
+    { 0x0049, -1 },  // (First char = [0x0049, I], Second char = [0x002D, -], Kerning dist = -1)
+    { 0x0054, -3 },  // (First char = [0x0054, T], Second char = [0x002D, -], Kerning dist = -3)
+    { 0x0046,  1 },  // (First char = [0x0046, F], Second char = [0x003F, ?], Kerning dist = 1)
+    { 0x0054,  1 },  // (First char = [0x0054, T], Second char = [0x003F, ?], Kerning dist = 1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x0043, C], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x0047, G], Kerning dist = -1)
+    { 0x002D, -1 },  // (First char = [0x002D, -], Second char = [0x0049, I], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x004F, O], Kerning dist = -1)
+    { 0x002D, -3 },  // (First char = [0x002D, -], Second char = [0x0054, T], Kerning dist = -3)
+    { 0x0044, -1 },  // (First char = [0x0044, D], Second char = [0x0054, T], Kerning dist = -1)
+    { 0x0046,  1 },  // (First char = [0x0046, F], Second char = [0x0054, T], Kerning dist = 1)
+    { 0x004F, -1 },  // (First char = [0x004F, O], Second char = [0x0054, T], Kerning dist = -1)
+    { 0x0054, -1 },  // (First char = [0x0054, T], Second char = [0x0054, T], Kerning dist = -1)
+};
